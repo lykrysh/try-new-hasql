10aa3d89076cc922fbe1319cf2632af1f2e2580e
lykrysh <lykrysh@email.com>
2018-06-16 14:39:43 -0700

    Connect to DB

--- src/Main.hs
+++ src/Main.hs
@@ -6,19 +6,23 @@ import Web.Spock.Config
 import Web.Spock.Lucid (lucid)
 import Lucid
 import Data.IORef (IORef, newIORef, atomicModifyIORef')
+import qualified Data.ByteString as DB
+import qualified Data.ByteString.Char8 as DBC
 import Control.Monad.IO.Class (liftIO)
+import Hasql.Connection
 
 data MySession =        EmptySession
 data MyAppState =       DummyAppState (IORef Int)
 
-type MyM =              SpockM () MySession MyAppState ()
-type MyAction =         SpockAction () MySession MyAppState ()
---type MyActionCtx ctx =  SpockActionCtx ctx () MySession MyAppState ()
+type MyDb =             Connection
+type MyM =              SpockM MyDb MySession MyAppState ()
+type MyAction =         SpockAction MyDb MySession MyAppState ()
+--type MyActionCtx ctx =  SpockActionCtx ctx MyDb MySession MyAppState ()
 
 main :: IO ()
 main = do
   ref <- newIORef 0
-  cfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
+  cfg <- defaultSpockCfg EmptySession (PCConn $ hasqlPool setting) (DummyAppState ref)
   runSpock 8080 (spock cfg app)
 
 app :: MyM
@@ -31,8 +35,18 @@ rootAction = do
   visitNum <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
   doCtx visitNum
 
-doCtx :: Int -> SpockActionCtx () () MySession MyAppState ()
+doCtx :: Int -> SpockActionCtx () MyDb MySession MyAppState ()
 doCtx i =
   lucid $ do
     h1_ "Hello"
     h1_ $ toHtml $ show i
+
+setting :: DB.ByteString
+setting = "host=localhost dbname=hasqldb port=5432 user=hasqluser password=password"
+
+hasqlPool :: Settings -> ConnBuilder Connection
+hasqlPool setting = ConnBuilder
+  { cb_createConn = either (error . show . fmap DBC.unpack) id <$> acquire setting
+  , cb_destroyConn = release
+  , cb_poolConfiguration = PoolCfg 10 1000 30
+  }
