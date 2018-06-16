0303ac6445c9cf535b5bf9a37d5bf7f75a26c80c
lykrysh <lykrysh@email.com>
2018-06-16 13:21:49 -0700

    Get default spock and lucid

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+
+import Web.Spock
+import Web.Spock.Config
+import Web.Spock.Lucid (lucid)
+import Lucid
+import Data.IORef (IORef, newIORef)
+
+data MySession =        EmptySession
+data MyAppState =       DummyAppState (IORef Int)
+
+type MyM =              SpockM () MySession MyAppState ()
+type MyAction =         SpockAction () MySession MyAppState ()
+--type MyActionCtx ctx =  SpockActionCtx ctx () MySession MyAppState ()
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  ref <- newIORef 0
+  cfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
+  runSpock 8080 (spock cfg app)
+
+app :: MyM
+app = do
+  get root rootAction
+
+rootAction :: MyAction
+rootAction = do
+  doCtx
+
+doCtx :: SpockActionCtx () () MySession MyAppState ()
+doCtx =
+  lucid $ do
+    h1_ "Hello"
