9b4e07c93c36202fa6ab1cfd0d8717b01e340286
lykrysh <lykrysh@email.com>
2018-06-17 05:00:47 -0700

    Configure initial info

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,65 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import qualified Data.Configurator as C
+import qualified Data.ByteString.Char8 as B 
+import Data.Text
+import Data.Int (Int32)
+import Data.IORef (IORef, newIORef, atomicModifyIORef')
+
 import Web.Spock
 import Web.Spock.Config
 import Web.Spock.Lucid (lucid)
 import Lucid
 import Lucid.Html5
-import Data.IORef (IORef, newIORef, atomicModifyIORef')
 import Control.Monad.IO.Class (liftIO)
 import Network.Wai.Middleware.Static (staticPolicy, addBase)
-import qualified Db as Db
+import Hasql.Connection
 
-data MySession =        EmptySession
+type MyDb =             Connection
+type MyM =              SpockM MyDb MySession MyAppState ()
+type MyAction =         SpockAction MyDb MySession MyAppState ()
+type MyActionCtx ctx =  SpockActionCtx ctx MyDb MySession MyAppState ()
+
+-- the session type is user id
+-- empty when not connected
+
+data MySession =
+  EmptySession
+    | SessionId Int32
+    deriving (Show, Eq, Ord)
+
 data MyAppState =       DummyAppState (IORef Int)
 
-type MyM =              SpockM Db.MyDb MySession MyAppState ()
-type MyAction =         SpockAction Db.MyDb MySession MyAppState ()
---type MyActionCtx ctx =  SpockActionCtx ctx Db.MyDb MySession MyAppState ()
+data Info = Info
+  { cfgTitle :: Text
+    , cfgDesc :: Text
+    , cfgDomain :: Text
+    , cfgDbConnStr :: B.ByteString
+  } deriving (Show, Eq, Ord)
+
+parseInfo :: FilePath -> IO (Info)
+parseInfo file = do
+  cfg <- C.load $ [C.Required file]
+  name <- C.require cfg "name"
+  desc <- C.require cfg "description"
+  domain <- C.require cfg "domain"
+  db <- C.require cfg "db"
+  pure (Info name desc domain db)
+
+hasqlPool :: B.ByteString -> ConnBuilder Connection
+hasqlPool setting = ConnBuilder
+  { cb_createConn = either (error . show . fmap B.unpack) id <$> acquire setting
+  , cb_destroyConn = release
+  , cb_poolConfiguration = PoolCfg 10 1000 30
+  }
 
 main :: IO ()
 main = do
+  info <- parseInfo "site.config"
+  let dbconn = cfgDbConnStr info
   ref <- newIORef 0
-  cfg <- defaultSpockCfg EmptySession (PCConn $ Db.hasqlPool Db.setting) (DummyAppState ref)
+  cfg <- defaultSpockCfg EmptySession (PCConn $ hasqlPool dbconn) (DummyAppState ref)
   runSpock 8080 (spock cfg app)
 
 app :: MyM
@@ -35,7 +73,7 @@ rootAction = do
   visitNum <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
   doCtx visitNum
 
-doCtx :: Int -> SpockActionCtx () Db.MyDb MySession MyAppState ()
+doCtx :: Int -> MyActionCtx ()
 doCtx i =
   lucid $ do
     doctypehtml_ $ do
@@ -45,3 +83,4 @@ doCtx i =
       body_ $ do
         h1_ "Hello"
         h1_ $ toHtml $ show i
+
