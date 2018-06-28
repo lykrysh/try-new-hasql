34256ed73ab0d11b255b680d8946683b12088c72
lykrysh <lykrysh@email.com>
2018-06-28 03:36:29 -0700

    Render ajax-ed buttons

--- src/Render/Base.hs
+++ src/Render/Base.hs
@@ -22,6 +22,7 @@ renderTemplate = do
     meta_ [charset_ "utf-8" ]
     head_ $ do
       link_ [ rel_ "stylesheet", type_ "text/css", href_ "/css/base.css" ]
+      script_ [ type_ "text/javascript", src_ "/js/jquery-3.2.1.min.js" ] (mempty :: Text)
     body_ $ do
       p_ "dummy"
 
@@ -44,6 +45,7 @@ renderNewFilms filmList = do
           td_ $ toHtml (signature f)
           td_ $ toHtml (author f)
           td_ $ toHtml $ show (year f)
+      h1_ "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
 
 renderSearchedFilms :: (Either Error [Film]) -> HT
 renderSearchedFilms filmList = div_ [ class_ "searched" ] $ do
--- src/Render/Filters.hs
+++ src/Render/Filters.hs
@@ -12,5 +12,19 @@ type HT = Html ()
 
 renderFilters :: HT
 renderFilters = do
-  div_ [ class_ "kwcontainer" ] $ do
-    h1_ "whatever"
+  div_ [ class_ "kwcontainer noselect" ] $ do
+    a_ [ data_ "stateName" "nonfiction", class_ "kwbutcommon but pntcursor", id_ "nonfiction" ] $ do
+      "Nonfiction"
+    a_ [ data_ "stateName" "fiction", class_ "kwbutcommon but pntcursor", id_ "fiction" ] $ do
+      "Fiction"
+    a_ [ data_ "stateName" "surreal", class_ "kwbutcommon but pntcursor", id_ "surreal" ] $ do
+      "Surreal"
+    a_ [ data_ "stateName" "abstraction", class_ "kwbutcommon but pntcursor", id_ "abstraction" ] $ do
+      "Abstraction"
+    a_ [ data_ "stateName" "soundscape", class_ "kwbutcommon but pntcursor", id_ "soundscape" ] $ do
+      "Soundscape"
+  div_ [ id_ "viewall" ] ""
+  div_ [ id_ "info" ] $ do
+    h1_ "Something else"
+  script_ [ type_ "text/javascript", src_ "/js/ajax.js" ] (mempty :: Text)
+
